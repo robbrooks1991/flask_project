521e315c994266d0f8cb9e6953b314736fd85094
Robert Brooks <brooksr144@gmail.com>
2017-06-06 14:37:15 +0100

    Connect to MongoDB

--- school_donations.py
+++ school_donations.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
@@ -5,10 +6,13 @@ import json
 
 app = Flask( __name__ )
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
+#MONGODB_HOST = 'localhost'
+#MONGODB_PORT = 27017
+#DBS_NAME = 'donorsUSA'
+
 COLLECTION_NAME = 'projects'
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
 
 
 @app.route( "/" )
@@ -35,12 +39,13 @@ def donor_projects():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient( MONGODB_HOST, MONGODB_PORT ) as conn:
+    # The MONGO_URI connection is required when hosted using a remote mongo db.
+    with MongoClient( MONGO_URI ) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
-        # and limit the the results to 55000
-        projects = collection.find( projection=FIELDS, limit=55000 )
+        # and limit the the results to a lower limit of 20000
+        projects = collection.find( projection=FIELDS, limit=20000 )
         # Convert projects to a list in a JSON object and return the JSON data
         return json.dumps( list( projects ) )
 
